9c78e213418dc380077a0c5f1579b7b226657d4a
Stephen <stephennnabuike@gmail.com>
2022-09-28 13:47:06 +0100

    Implement review changes

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative './person'
 
 class Teacher < Person
   def initialize(specialization, age, name)
-    super(age, name)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
